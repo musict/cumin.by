580eba7f934caf3f2ff72c8dbff76107e24291b2
Волков Сергей Викторович <VolkovSV@rrb.by>
2019-06-06 10:15:15 +0300

    настройка сообщения

--- send.php
+++ send.php
@@ -10,7 +10,7 @@ $name = trim($name);
 $phone = trim($phone);
 
 if (mail("cuminby@mail.ru", "Заявка с сайта", "Имя:  ".$name. " | Телефон:  ".$phone." | Сообщение: ".$massage." \r\n"))
- {     header("Location: /thanx.html");
+ {     header("Location: /blog.html");
        exit;
 } else {
     echo "Возникла ошибка, перезвоните нам, пожалуйста 8 029 768 93 18 ";
