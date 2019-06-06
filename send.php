76c4cf3d91ef3546ff424c5e657e9110a2c5890c
Волков Сергей Викторович <VolkovSV@rrb.by>
2019-06-06 10:24:58 +0300

    mail

--- send.php
+++ send.php
@@ -9,7 +9,7 @@ $phone = urldecode($phone);
 $name = trim($name);
 $phone = trim($phone);
 
-if (mail("comtehnotorg@gmail.com", "Заявка с сайта", "Имя:  ".$name. " | Телефон:  ".$phone." | Сообщение: ".$massage." \r\n"))
+if (mail("cuminby@mail.ru", "Заявка с сайта", "Имя:  ".$name. " | Телефон:  ".$phone." | Сообщение: ".$massage." \r\n"))
  {     header("Location: /blog.html");
        exit;
 } else {
