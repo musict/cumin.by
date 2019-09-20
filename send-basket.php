2193608cb7015a646470cbb989d29a630ce82c05
Волков Сергей Викторович <VolkovSV@rrb.by>
2019-09-20 15:48:13 +0300

    форма

--- send-basket.php
+++ send-basket.php
@@ -2,23 +2,23 @@
 $phone = $_POST['phone'];
 $name = $_POST['name'];
 $time = $_POST['time'];
+//
+//
+// $phone = htmlspecialchars($phone);
+// $name = htmlspecialchars($name);
+// $time = htmlspecialchars($time);
+//
+//
+// $phone = urldecode($phone);
+// $name = urldecode($name);
+// $time = urldecode($time);
+//
+// $phone = trim($phone);
+// $name = trim($name);
+// $time = trim($time);
 
 
-$phone = htmlspecialchars($phone);
-$name = htmlspecialchars($name);
-$time = htmlspecialchars($time);
-
-
-$phone = urldecode($phone);
-$name = urldecode($name);
-$time = urldecode($time);
-
-$phone = trim($phone);
-$name = trim($name);
-$time = trim($time);
-
-
-if (mail("cuminby@mail.ru", "Заявка с сайта", "Имя:  ".$name. " | Телефон:  ".$phone." | Сообщение: ".$time." \r\n"))
+if (mail("cuminby@mail.ru", "Заявка с сайта", "Имя:  ".$name. " | Телефон:  ".$phone." | Сообщение: ".$time." "))
  {     header("Location: /thanks.html");
        exit;
 } else {
