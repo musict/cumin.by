348197d2e80c12ff34429ecf6f2d45c2f0829f6f
Волков Сергей Викторович <VolkovSV@rrb.by>
2019-09-20 15:58:36 +0300

    тест

---
+++ send-basket.php
@@ -0,0 +1,26 @@
+<?php
+// $phone = $_POST['phone'];
+// $name = $_POST['name'];
+$time = $_POST['time'];
+
+
+// $phone = htmlspecialchars($phone);
+// $name = htmlspecialchars($name);
+// $time = htmlspecialchars($time);
+//
+//
+// $phone = urldecode($phone);
+// $name = urldecode($name);
+// $time = urldecode($time);
+//
+// $phone = trim($phone);
+// $name = trim($name);
+// $time = trim($time);
+
+
+if (mail("cuminby@mail.ru", "Заявка с сайта", "Сообщение: ".$time." "))
+ {     header("Location: /thanks.html");
+       exit;
+} else {
+    echo "Возникла ошибка, перезвоните нам, пожалуйста 8 029 768 93 18 ";
+}?>
