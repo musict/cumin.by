a71246a2b9a286a27982a9be0fd7b1acdb77cdea
musict <musicy@yandex.ru>
2020-12-01 22:29:14 +0300

    add recaptcha

--- send.php
+++ send.php
@@ -1,17 +1,43 @@
 <?php
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-$name = htmlspecialchars($name);
-$phone = htmlspecialchars($phone);
-$name = urldecode($name);
-$phone = urldecode($phone);
-$name = trim($name);
-$phone = trim($phone);
+if (isset($_POST['g-recaptcha-response'])) $captcha_response = $_POST['g-recaptcha-response'];
+else die('На форме нет капчи! Обратитесь к администратору!');
+$url = 'https://www.google.com/recaptcha/api/siteverify';
+$params = [
+    'secret' => '6LejOvUZAAAAANFGcHsBH5rLPd55HiKcdaqTWaSs',
+    'response' => $captcha_response,
+    'remoteip' => $_SERVER['REMOTE_ADDR']
+];
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($ch);
+if(!empty($response)) $decoded_response = json_decode($response);
+$success = false;
+if ($decoded_response && $decoded_response->success)
+{
+    $success = $decoded_response->success;
+}
 
-if (mail("cuminby@mail.ru", "Заявка с сайта", "Имя:  ".$name. " | Телефон:  ".$phone." | Сообщение: ".$message." \r\n"))
- {     header("Location: /thanks.html");
-       exit;
-} else {
-    echo "Возникла ошибка, перезвоните нам, пожалуйста 8 029 768 93 18 ";
-}?>
+if ($result = $success)
+	{
+	  	$name = $_POST['name'];
+		$phone = $_POST['phone'];
+		$message = $_POST['message'];
+		$name = htmlspecialchars($name);
+		$phone = htmlspecialchars($phone);
+		$name = urldecode($name);
+		$phone = urldecode($phone);
+		$name = trim($name);
+		$phone = trim($phone);
+
+		if (mail("cuminby@mail.ru", "Заявка с сайта", "Имя:  ".$name. " | Телефон:  ".$phone." | Сообщение: ".$message." \r\n"))
+		 {     header("Location: /thanks.html");
+		       exit;
+		} else {
+		    echo "Возникла ошибка, перезвоните нам, пожалуйста 8 029 768 93 18 ";
+		}
+	}
+else die('Вы не прошли проверку "Я не робот"');
